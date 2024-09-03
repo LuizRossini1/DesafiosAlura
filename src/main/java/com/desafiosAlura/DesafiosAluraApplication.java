bcf9af3b68348085e73a3fee6bf46af0c5b69e77
LuizRossini <luiz.guirossini123@gmail.com>
2024-09-03 19:27:33 -0300

    first commit

---
+++ src/main/java/com/desafiosAlura/DesafiosAluraApplication.java
@@ -0,0 +1,10 @@
+package com.desafiosAlura;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosAluraApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiosAluraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/desafiosAlura/DesafiosAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiosAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
